d827be28c97a4b0c97e10d8c82f2d6effdab9b10
anoh <33670143+anohr@users.noreply.github.com>
2023-12-12 13:38:22 +0100

    spell

--- instapot/src/image/LikedImages.tsx
+++ instapot/src/image/LikedImages.tsx
@@ -23,7 +23,7 @@ const LikedImages = () => {
   } else {
     return (
       <div className='flex flex-col gap-3'>
-        <div className='border-t border-gray-200 p-3 bg-white rounded text-black flex flex-col gap-2 italic'>Sorry, inget gillade bilder... </div>
+        <div className='border-t border-gray-200 p-3 bg-white rounded text-black flex flex-col gap-2 italic'>Sorry, inga gillade bilder... </div>
       </div>
     );
   }
