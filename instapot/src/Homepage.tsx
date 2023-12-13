494d694f29d7f1b1140d3089f26576b11cf07445
anoh <33670143+anohr@users.noreply.github.com>
2023-12-13 11:58:36 +0100

    padding

--- instapot/src/Homepage.tsx
+++ instapot/src/Homepage.tsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <>
       <Sidenav />
-      <main className='py-5'>
+      <main className='py-5 pb-20 lg:py-5'>
         <Outlet />
       </main>
     </>
